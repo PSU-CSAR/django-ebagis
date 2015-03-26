ea9da67663666203a2ca6a61327a7b9f57284fbe
Jarrett Keifer <jarrettkeifer@gmail.com>
2015-03-26 00:24:10 -0700

    batch file for starting celery task in progress

--- setup.bat
+++ setup.bat
@@ -2,12 +2,22 @@
 
 Setlocal EnableDelayedExpansion
 
+
+REM *****constants******
+REM --------------------
+set anaconda_path=C:\Anaconda_x86
+set arcgis_path=C:\Program Files (x86)\ArcGIS\Desktop10.3
+set arcpy_pth_filename=desktop10.3.pth
+
+
 REM create secret key
+REM -----------------
 set command='python -c "import random, string; print ''.join([random.SystemRandom().choice('{}{}{}'.format(string.ascii_letters, string.digits, string.punctuation.replace('\'', '').replace('\\', ''))) for i in range(50)])"'
-echo %command%
 for /f %%i in (%command%) do set SECRET_KEY=%%i
 
+
 REM clone the django-bagis repo
+REM ---------------------------
 git clone git@github.com:PSU-CSAR/django-ebagis.git ../django-ebagis && (
   echo cloned django-ebagis with push permissions
 ) || (
@@ -15,24 +25,30 @@ git clone git@github.com:PSU-CSAR/django-ebagis.git ../django-ebagis && (
   git clone https://github.com/PSU-CSAR/django-ebagis.git ../django-ebagis
 )
 
+
 REM create env and install conda packages
+REM -------------------------------------
 conda create -n %1 python --file conda-requirements.txt
 call activate %1
 
+
 REM install pip dependencies
+REM ------------------------
 set OLDDIR=%CD%
 cd ../django-ebagis
 pip install -r ../django-ebagis/requirements.txt
 cd %OLDDIR%
 pip install -r requirements.txt
 
+
 REM create database
+REM ---------------
 echo %3 | createdb -U %2 -D gis_data -T postgis_21 %1
 
+
 REM create secret file
+REM ------------------
 set secretfile=ebagis_site\secret.py
-echo %secretfile%
-echo %secretfile%
 @echo SECRET_KEY = '!SECRET_KEY!' > %secretfile%
 @echo DATABASE_SETTINGS = { >> %secretfile%
 @echo     'default': { >> %secretfile%
@@ -45,11 +61,21 @@ echo %secretfile%
 
 
 REM copy pth file for arcpy
-set pthfile=C:\Anaconda_x86\envs\%1\Lib\site-packages\desktop10.3.pth
-@echo C:\Program Files (x86)\ArcGIS\Desktop10.3\bin>%pthfile%
-@echo C:\Program Files (x86)\ArcGIS\Desktop10.3\ArcPy>>%pthfile%
-@echo C:\Program Files (x86)\ArcGIS\Desktop10.3\ArcToolBox\Scripts>>%pthfile%
+REM -----------------------
+set pthfile=%anaconda_path%\envs\%1\Lib\site-packages\%arcpy_pth_filename%
+@echo %arcgis_path%\bin>%pthfile%
+@echo %arcgis_path%\ArcPy>>%pthfile%
+@echo %arcgis_path%\ArcToolBox\Scripts>>%pthfile%
 
-REM migrate database and load users
+
+REM create batch file for celery task
+REM ---------------------------------
+REM set env_python=%anaconda_path%\envs\%1\python.exe
+REM @echo %env_python% -m celery.bin.celeryd>start_celery.bat
+
+
+REM migrate database and load user fixtures
+REM ---------------------------------------
+python manage.py flush
 python manage.py migrate
 python manage.py loaddata ebagis_site\fixtures\user_fixtures.json
