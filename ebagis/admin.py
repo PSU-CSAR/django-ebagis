1792e865f6a095925d89011f7a2be2e7e1e44c11
Jarrett Keifer <jarrettkeifer@gmail.com>
2015-07-03 19:55:00 -0700

    Don't think I need this...keeping commeted for now

--- ebagis/admin.py
+++ ebagis/admin.py
@@ -1,17 +1,18 @@
 from django.contrib.gis import admin
-from .models import AOI
-from .models import Surfaces, Layers, Prism, AOIdb, HRUZones
-from .models import XML, Raster, Vector, Table
-from .models import Maps
-
-admin.site.register(AOI, admin.GeoModelAdmin)
-admin.site.register(Surfaces, admin.ModelAdmin)
-admin.site.register(Layers, admin.ModelAdmin)
-admin.site.register(Prism, admin.ModelAdmin)
-admin.site.register(AOIdb, admin.ModelAdmin)
-admin.site.register(HRUZones, admin.ModelAdmin)
-admin.site.register(XML, admin.ModelAdmin)
-admin.site.register(Raster, admin.ModelAdmin)
-admin.site.register(Vector, admin.ModelAdmin)
-admin.site.register(Table, admin.ModelAdmin)
-admin.site.register(Maps, admin.ModelAdmin)
+# I don't think I need this really
+#from .models.aoi import AOI
+#from .models.geodatabase import Surfaces, Layers, Prism, AOIdb, HRUZones
+#from .models.file import File, XML, Raster, Vector, Table
+#from .models.directory import Maps, PrismDir
+#
+#admin.site.register(AOI, admin.GeoModelAdmin)
+#admin.site.register(Surfaces, admin.ModelAdmin)
+#admin.site.register(Layers, admin.ModelAdmin)
+#admin.site.register(Prism, admin.ModelAdmin)
+#admin.site.register(AOIdb, admin.ModelAdmin)
+#admin.site.register(HRUZones, admin.ModelAdmin)
+#admin.site.register(XML, admin.ModelAdmin)
+#admin.site.register(Raster, admin.ModelAdmin)
+#admin.site.register(Vector, admin.ModelAdmin)
+#admin.site.register(Table, admin.ModelAdmin)
+#admin.site.register(Maps, admin.ModelAdmin)
