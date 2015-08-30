31541ef00785fc944edd73a37f228779deb4a19a
jkeifer <jarrettkeifer@gmail.com>
2015-08-30 16:01:13 -0700

    refactoring, comments, and revamping of utilities.py

--- ebagis/utilities.py
+++ ebagis/utilities.py
@@ -36,6 +36,7 @@ def generate_random_name(layershortname, ext=""):
     return layershortname + "_" + random_string() + ext
 
 
+# I THINK THIS IS DEAD CODE; VERIFY AND REMOVE
 def get_pg_srid(psycopgcursor, spatialref, test="WKT"):
     """
     """
@@ -53,6 +54,7 @@ def get_pg_srid(psycopgcursor, spatialref, test="WKT"):
         return None
 
 
+# I THINK THIS IS DEAD CODE; VERIFY AND REMOVE
 def srid_of_spatialref(psycopgcursor, spatialref):
     """
     """
@@ -68,22 +70,22 @@ def srid_of_spatialref(psycopgcursor, spatialref):
 
 
 def get_multipart_wkt_geometry(sourcefile, layername=None):
-    """
-    """
+    """extract a single multipolygon geometry from an sourcefile
+    containing polygon geometries"""
     from osgeo import ogr
 
     ogr.UseExceptions()
 
     # open sourcefile using OGR
-    shapefile = ogr.Open(sourcefile)
+    dataset = ogr.Open(sourcefile)
 
-    if not shapefile:
+    if not dataset:
         raise Exception("Failed to open file {}.".format(sourcefile))
 
     if layername:
-        layer = shapefile.GetLayerByName(layername)
+        layer = dataset.GetLayerByName(layername)
     else:
-        layer = shapefile.GetLayer(0)
+        layer = dataset.GetLayer(0)
 
     crs_wkt = layer.GetSpatialRef().ExportToWkt()
 
@@ -113,6 +115,8 @@ def get_multipart_wkt_geometry(sourcefile, layername=None):
 
 
 def wkt_polygons_to_multipolygon(wkt_polygons):
+    """put multiple wkt polygons together to form a single
+    multipolygon wkt geometry"""
     cleaned_wkts = []
 
     for wkt in wkt_polygons:
@@ -124,8 +128,10 @@ def wkt_polygons_to_multipolygon(wkt_polygons):
         else:
             raise Exception("Invalid or unknown geometry type detected.")
 
+        # append the geometry to this list, sans geometry type tag
         cleaned_wkts.append(wkt)
 
+    # join the individual geometeries in the list into a single multipolygon
     return "MULTIPOLYGON (" + ",".join(cleaned_wkts) + ")"
 
 
@@ -142,8 +148,6 @@ def get_authority_code_from_spatial_ref(spatial_ref):
 
 
 def create_spatial_ref_from_EPSG(epsg_code):
-    """
-    """
     from osgeo import osr
     spatial_ref = osr.SpatialReference()
     spatial_ref.ImportFromEPSG(epsg_code)
@@ -151,8 +155,6 @@ def create_spatial_ref_from_EPSG(epsg_code):
 
 
 def create_spatial_ref_from_wkt(wkt):
-    """
-    """
     from osgeo import osr
     spatial_ref = osr.SpatialReference()
     spatial_ref.ImportFromWkt(wkt)
@@ -165,24 +167,107 @@ def get_seq_next_val(connection, sequence):
             return cursor.fetchall()[0][0]
 
 
-def make_unique_directory(shortname, data_repository):
+def generate_postfix(iteration, sequential=False):
+    """used to generate the postfixes for the
+    make_unique_directory function"""
+    # a sequential postfix uses integers
+    if sequential:
+        postfix = "_" + iteration
+    else:
+        # otherwise we append a unique string
+        # the length of the string is 4 char,
+        # but increases by 1 every 100 iterations
+        length = 4 + (1 * (iteration % 100))
+        postfix = "_" + random_string(length)
+    return postfix
+
+
+def make_unique_directory(name, path,
+                          limit=None,
+                          sequential_postfix=False,
+                          always_append=False):
+    """Iterate though postfixes to a directory name until
+    the name is unique and the directory can be created. If
+    the original name is unique no postfix will be appended,
+    unless the always_append option is set to True, in which
+    case a postfix will be appended, even if the name was
+    already unique.
+
+    User can set a limit value to stop trying to create the
+    directory after that many failed attempts. The default is
+    to run an unlimited number of times.
+
+    User can also choose to use a sequential postfix, which
+    appends an integer to the file name, starting with 1, and
+    increments the integer until the limit is reached. Note that
+    combining the sequential_postfix and always_append options
+    will not start at 1, but at 0. The default behavior is to use
+    a random string which is minimally 4 characters. Every 100
+    iterations the string will add an extra letter to attempt to
+    avoid collisions and make the directory more quickly.
+
+    The two required arguments to this function are name and path,
+    which are both strings. Name will be used as the starting point
+    for the name of the directory to be created, and path is the
+    location at which the directory should be created. That is,
+    with a name of `bar` and a path of `/home/foo/`, the function
+    will attempt to create the directory `/home/foo/bar`, appending
+    a postfix as desribed above until the directory can be created."""
     import os
 
-    while True:
-        uniqueid = shortname + "_" + random_string()
+    # if no limit then set to True so while loop will never stop
+    if limit is None:
+        limit = True
+    # if limit is a postive integer, don't need to do anything
+    else if type(limit) == int and limit > 0:
+        continue
+    # if limit is neither of these then we don't know what to do
+    else:
+        raise InputError("limit is not positive integer or None. Unable to proceed")
+
+    # postfix starts as empty string unless always_append is True
+    postfix = ""
+    if always_append is True:
+        postfix = generate_postfix(0, sequential_postfix)
+
+    # iterate through names until directory is created
+    while limit:
+        iteration += 1
+
+        # append postfix and create full directory path
+        uniqueid = name + postfix
         outdirectory = os.path.join(data_repository, uniqueid)
 
+        # if the directory is made then break the while loop
+        # so the function will return. Failure will result in
+        # an exception caught by the try, which will indicate
+        # the loop needs to continue.
         try:
             os.mkdir(outdirectory)
             break
         except OSError:
             pass
 
+        # if limit is an integer, then we need to subtract one
+        # then we need to check if limit is now 0. If so,
+        # we need to raise an exception indicating failure.
+        if not limit is True:
+            limit -= 1
+            if limit <= 0:
+                raise LimitError("Postfix limit was reached before directory could be created.")
+
+        # generate the next postfix based on the iteration
+        postfix = generate_postfix(iteration, sequential_postfix)
+
     return uniqueid, outdirectory
 
 
 def validate_path(path, allow_whitespace=False,
                   invalid_chars=[":", "/", "\\", "*", "?", ".", "%", "$"]):
+    """Validate a user-given path to ensure it does not have any
+    restricted characters that could be used for malicious intent,
+    or for non-malicious intent that might result in undesired
+    operation."""
     if not allow_whitespace:
         from string import whitespace
         for char in whitespace:
@@ -199,13 +284,18 @@ def validate_path(path, allow_whitespace=False,
 
 
 def zip_directory(directory_path, zip_path):
+    """Takes an input directory path and an output zip path
+    and zips the contents of the directory into a zipfile at
+    output path location. The name of the output zipfile
+    should be included in the zip_path argument."""
     import os
     import zipfile
     with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(directory_path):
             for f in files:
                 rel_dir = os.path.relpath(root, directory_path)
-                # first arg is file to zip, second arg is archive name
+                # first arg is file to zip (root/f), and the
+                # second arg is the archive name (rel_dir/f)
                 # (second arg is path realtive to the root of the AOI
                 # to keep whole higher level directory structure from
                 # getting zipped)
@@ -218,9 +308,9 @@ def zip_directory(directory_path, zip_path):
 
 @contextmanager
 def tempdirectory(suffix="", prefix="", dir=None):
-    """A context manager for creating and then
-    deleting a temporary directory.
-    """
+    """A context manager for creating a temporary directory
+    that will automatically be removed when it is no longer
+    in context."""
     from tempfile import mkdtemp
     from shutil import rmtree
     tmpdir = mkdtemp(suffix=suffix, prefix=prefix, dir=dir)
@@ -231,3 +321,22 @@ def tempdirectory(suffix="", prefix="", dir=None):
     finally:
         rmtree(tmpdir)
         pass
+
+
+def get_queryset_arguments(object):
+    """This function is used by some of the view functions to
+    determine what URL arguments are to be used to filter instance
+    queries. For example, finding all layers from a geodatabase in
+    a specific AOI, the goedatabase and AOI ids would be used as
+    the filters."""
+    query_dict = {}
+    for kwarg in object.kwargs:
+        if kwarg.startswith(URL_FILTER_QUERY_ARG_PREFIX):
+            query_lookup = kwarg.replace(
+                URL_FILTER_QUERY_ARG_PREFIX,
+                '',
+                1
+            )
+            query_value = object.kwargs.get(kwarg)
+            query_dict[query_lookup] = query_value
+    return query_dict
