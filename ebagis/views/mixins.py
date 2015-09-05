4bd0fded680ab04ef2d95c889f2737a13984218b
jkeifer <jarrettkeifer@gmail.com>
2015-09-05 15:30:37 -0700

    generic mixin for upload/download methods on model viewsets re #5

--- ebagis/views/mixins.py
+++ ebagis/views/mixins.py
@@ -24,3 +24,26 @@ class MultiSerializerViewSet(viewsets.ModelViewSet):
         return self.serializers.get(self.action,
                                     self.serializers['default'])
 
+
+class LoadableViewSet(viewsets.ModelViewSet):
+    """Provides generic create, update, and download
+    views for a model object that supports such "load"
+    operations."""
+
+    @property
+    def _model(self):
+        return self.queryset.model
+
+    def create(self, request, *args, **kwargs):
+        return UploadView.new_upload(self._model, request)
+
+    @detail_route
+    def update(self, request, *args, **kwargs):
+        object = self.get_object()
+        return UploadView.new_upload(self._model, request, object_id=object.id)
+
+    @detail_route()
+    def download(self, request, *args, **kwargs):
+        object = self.get_object()
+        return DownloadViewSet.new_download(object, request)
+
