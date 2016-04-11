d780b24ff4bf22a45926814e0e2f6e92cc03b277
Jarrett Keifer <jarrettkeifer@gmail.com>
2016-04-10 17:40:11 -0700

    fixed show_all to make it flag

--- ebagis/utils/queries.py
+++ ebagis/utils/queries.py
@@ -1,24 +1,13 @@
 from __future__ import absolute_import
 
-from distutils.util import strtobool
-
 
 def admin_queryset_filter(queryset, request):
     """
     Get (and filter) Upload queryset.
     By default, user can only continue uploading his/her own uploads.
     """
-    show_all = False
 
-    try:
-        # use strtobool to accept a variety of strings repr true/false
-        show_all = strtobool(request.query_params.get("show_all", "false"))
-    except ValueError:
-        # the string was not a recognized truth string,
-        # so keep show_all as false
-        pass
-
-    if show_all and request.user.is_staff:
+    if "show_all" in request.query_params and request.user.is_staff:
         # the user is admin and wants to see all records,
         # so don't filter by user
         return queryset
