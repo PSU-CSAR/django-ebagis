2a8db6fce354549332c004f9d88776caa4a3464a
Jarrett Keifer <jarrettkeifer@gmail.com>
2017-03-07 20:01:42 -0800

    fix up permissions/groups/users serializers

--- ebagis/serializers/user.py
+++ ebagis/serializers/user.py
@@ -6,35 +6,10 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ('url', 'username')
-
-
-class UserDetailSerializer(serializers.ModelSerializer):
-    roles = serializers.SerializerMethodField()
-
-    def get_roles(self, obj):
-        user = obj
-        roles = []
-
-        if user and user.is_staff:
-            roles.append("ROLE_STAFF")
-        if user and user.is_superuser:
-            roles.append("ROLE_ADMIN")
-
-        return roles
-
-    class Meta:
-        model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name', 'roles')
-        read_only_fields = ('email', 'roles')
-
-
 class PermissionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Permission
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -47,3 +22,47 @@ class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Group
+        fields = 'name'
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('url', 'username')
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    roles = serializers.SerializerMethodField()
+    groups = serializers.SerializerMethodField()
+
+    def get_roles(self, obj):
+        user = obj
+        roles = []
+
+        if user and user.is_staff:
+            roles.append("ROLE_STAFF")
+        if user and user.is_superuser:
+            roles.append("ROLE_ADMIN")
+
+        return roles
+
+    def get_groups(self, obj):
+        return [grp.name for grp in obj.groups.all()]
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'roles',
+            #'user_permissions',
+            'groups',
+        )
+        read_only_fields = (
+            'email',
+            'roles',
+            #'user_permissions',
+            'groups',
+        )
