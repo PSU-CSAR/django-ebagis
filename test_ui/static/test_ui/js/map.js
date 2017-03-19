6d4d9d8ce840dcb78d731416e8301ede2350d42c
Jarrett Keifer <jarrettkeifer@gmail.com>
2017-03-18 22:22:07 -0700

    clean up some unused code

--- test_ui/static/test_ui/js/map.js
+++ test_ui/static/test_ui/js/map.js
@@ -93,50 +93,6 @@ $('#feature-list').on('click', '.zoom-to-pp', function(e) {
     return false;
 });
 
-
-
-/*if ( !("ontouchstart" in window) ) {
-  $(document).on("mouseover", ".feature-row", function(e) {
-    highlight.clearLayers().addLayer(L.circleMarker([$(this).attr("lat"), $(this).attr("lng")], highlightStyle));
-  });
-}*/
-
-//$(document).on("mouseout", ".feature-row", clearHighlight);
-
-$("#about-btn").click(function() {
-  $("#aboutModal").modal("show");
-  $(".navbar-collapse.in").collapse("hide");
-  return false;
-});
-
-$("#full-extent-btn").click(function() {
-  map.fitBounds(boroughs.getBounds());
-  $(".navbar-collapse.in").collapse("hide");
-  return false;
-});
-
-$("#legend-btn").click(function() {
-  $("#legendModal").modal("show");
-  $(".navbar-collapse.in").collapse("hide");
-  return false;
-});
-
-$("#login-btn").click(function() {
-  $("#loginModal").modal("show");
-  $(".navbar-collapse.in").collapse("hide");
-  return false;
-});
-
-$("#list-btn").click(function() {
-  animateSidebar();
-  return false;
-});
-
-$("#nav-btn").click(function() {
-  $(".navbar-collapse").collapse("toggle");
-  return false;
-});
-
 $("#sidebar-toggle-btn").click(function() {
   animateSidebar();
   return false;
