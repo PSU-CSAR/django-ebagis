ca60eeb868f1849f7c5e993407be9af84a56cf93
Jarrett Keifer <jarrettkeifer@gmail.com>
2017-03-19 11:41:11 -0700

    make status field optional on Upload serializer (as is readonly and cannot be set by users)

--- ebagis/serializers/upload.py
+++ ebagis/serializers/upload.py
@@ -16,7 +16,7 @@ class UploadSerializer(serializers.ModelSerializer):
     user = serializers.HyperlinkedRelatedField(view_name="user-detail",
                                                read_only=True)
     task = TaskSerializer(read_only=True)
-    status = FriendlyChoiceField(choices=Upload.STATUS_CHOICES)
+    status = FriendlyChoiceField(choices=Upload.STATUS_CHOICES, required=False)
 
 # TODO: move the validation to the model classes, and call the necessary methods here
 #    def validate(self, data):
