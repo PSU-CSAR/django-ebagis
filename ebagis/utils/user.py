a2c90cb5379c5b4b241a162f30cad3053d15fe90
Jarrett Keifer <jarrettkeifer@gmail.com>
2017-04-09 17:14:52 -0700

    fix error when user token does not exist and user deactivated

--- ebagis/utils/user.py
+++ ebagis/utils/user.py
@@ -1,8 +1,14 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+
 def deactivate(user):
+    try:
+        user.token.delete()
+    except ObjectDoesNotExist:
+        pass
     user.is_active = False
     user.set_unusable_password()
     user.email = ''
     user.first_name = ''
     user.last_name = ''
     user.save()
-    user.token.delete()
