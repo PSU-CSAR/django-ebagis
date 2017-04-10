195b81341e0d264db0bfef33b6faabee099f4b0e
Jarrett Keifer <jarrettkeifer@gmail.com>
2017-04-09 17:38:49 -0700

    disable buggy debugging toolbar panels

--- ebagis_site/settings.py
+++ ebagis_site/settings.py
@@ -103,11 +103,11 @@ DEBUG_TOOLBAR_PANELS = [
     'debug_toolbar.panels.sql.SQLPanel',
     'debug_toolbar.panels.staticfiles.StaticFilesPanel',
     'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
+    #'debug_toolbar.panels.cache.CachePanel',
     'debug_toolbar.panels.signals.SignalsPanel',
     'debug_toolbar.panels.logging.LoggingPanel',
     'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
+    #'debug_toolbar.panels.profiling.ProfilingPanel',
 ]
 INTERNAL_IPS = '127.0.0.1'
 
