ac67f92c2483e703e2dae12a0cb528d981315938
Jarrett Keifer <jarrettkeifer@gmail.com>
2017-04-09 17:21:35 -0700

    additional cache settings

--- ebagis_site/settings.py
+++ ebagis_site/settings.py
@@ -219,13 +219,17 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
 # cache settings
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": not DEBUG,
         }
     }
 }
