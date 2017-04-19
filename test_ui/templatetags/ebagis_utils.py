a03293ae384ed2a2d9c387e435c0d2b42d23ce9e
Jarrett Keifer <jarrettkeifer@gmail.com>
2017-04-19 03:54:53 -0700

    forgot to commit templatetag for aoi details

--- test_ui/templatetags/ebagis_utils.py
+++ test_ui/templatetags/ebagis_utils.py
@@ -1,7 +1,17 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
+
 
 register = template.Library()
+
 
 @register.filter
 def dictsort_lower(lst, key_name):
     return sorted(lst, key=lambda item: getattr(item, key_name).lower())
+
+
+@register.filter
+def content_type(obj):
+    if not obj:
+        return None
+    return ContentType.objects.get_for_model(obj).id
