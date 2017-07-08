e90fc623ac7896d4b7cbbc4d19ac76fb58a3ad32
Jarrett Keifer <jarrettkeifer@gmail.com>
2017-07-08 16:46:11 -0700

    change default user permissions

--- ebagis/settings/rest.py
+++ ebagis/settings/rest.py
@@ -17,7 +17,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissions',
         'rest_framework.permissions.IsAdminUser',
-        'ebagis.permissions.IsOwner',
         ),
     'PAGINATE_BY': 100,
     'DEFAULT_METADATA_CLASS': 'rest_framework.metadata.SimpleMetadata',
